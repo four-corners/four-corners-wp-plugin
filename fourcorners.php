f635ba2973a69e3b5892a5e59186d2ad88ff60c4
coreytegeler <coreytegeler@gmail.com>
2019-06-06 13:41:30 -0400

    add init script

--- fourcorners.php
+++ fourcorners.php
@@ -2,17 +2,18 @@
 /**
  * Plugin Name: Four Corners for WordPress
  * Plugin URI: https://www.fourcornersproject.org
- * Description: The very first plugin that I have ever created.
+ * Description: Loads necessary scripts to easily publish Four Corners photos
  * Version: 0.0.1
  * Author: Corey Tegeler
  * Author URI: https://www.fourcornersproject.org
  */
 
 function four_corners_scripts() {
-
-	$ver = '0.0.1';
-	wp_enqueue_script( 'four_corners_script', plugin_dir_path() . '/fourcorners.js/dist/four-corners.min.js', array(), $ver, true );
-	wp_enqueue_style( 'four_corners_style', plugin_dir_path() . '/fourcorners.js/dist/four-corners.min.css' );
+	$plugin_ver = '0.0.1';
+	$lib_ver = '0.1.2';
+	wp_enqueue_script( 'four_corners_script', plugin_dir_path( 'fourcorners-wp-plugin.js' ), array(), $plugin_ver, true );
+	wp_enqueue_script( 'four_corners_script', plugin_dir_path( '/fourcorners.js/dist/four-corners.min.js' ), array(), $lib_ver, true );
+	wp_enqueue_style( 'four_corners_style', plugin_dir_path( '/fourcorners.js/dist/four-corners.min.css', $lib_ver ) );
 
 }
 
