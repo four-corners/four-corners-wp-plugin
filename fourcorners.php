327bc298bd3038c1cde100c4c0651d7df986b9b8
coreytegeler <coreytegeler@gmail.com>
2019-06-10 16:29:32 -0400

    ready for test

--- fourcorners.php
+++ fourcorners.php
@@ -11,8 +11,8 @@
 function four_corners_plugin_scripts() {
 	$plugin_ver = '0.0.1';
 	$lib_ver = '0.1.5';
+	wp_enqueue_style( 'four_corners_style', plugin_dir_url( __FILE__ ) . 'fourcorners.js/dist/fourcorners.min.css', array(), $lib_ver );
 	wp_enqueue_script( 'four_corners_script', plugin_dir_url( __FILE__ ) . 'fourcorners.js/dist/fourcorners.min.js', array(), $lib_ver, true );
-	wp_enqueue_style( 'four_corners_style', plugin_dir_url( __FILE__ ) . 'fourcorners.js/dist/fourcorners.min.css', $lib_ver );
 	wp_enqueue_script( 'four_corners_script', plugin_dir_url( __FILE__ ) . 'fourcorners-wp-plugin.js', array(), $plugin_ver, true );
 }
 
