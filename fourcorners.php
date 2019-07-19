bf69806ac31beee8955e4b3181210c2b47e2421c
coreytegeler <coreytegeler@gmail.com>
2019-07-19 19:11:19 -0400

    update lib to 0.2.0

--- fourcorners.php
+++ fourcorners.php
@@ -3,14 +3,14 @@
  * Plugin Name: Four Corners for WordPress
  * Plugin URI: https://www.fourcornersproject.org
  * Description: Loads necessary scripts to easily publish Four Corners photos
- * Version: 0.0.2
+ * Version: 0.0.3
  * Author: Corey Tegeler
  * Author URI: https://www.fourcornersproject.org
  */
 
 function four_corners_plugin_scripts() {
-	$plugin_ver = '0.0.2';
-	$lib_ver = '0.1.9';
+	$plugin_ver = '0.0.3';
+	$lib_ver = '0.2.0';
 	wp_enqueue_style( 'four_corners_style', plugin_dir_url( __FILE__ ) . 'fourcorners.js/dist/fourcorners.min.css', array(), $lib_ver );
 	wp_enqueue_script( 'four_corners_script', plugin_dir_url( __FILE__ ) . 'fourcorners.js/dist/fourcorners.min.js', array(), $lib_ver, true );
 	wp_enqueue_script( 'four_corners_plugin_script', plugin_dir_url( __FILE__ ) . 'fourcorners-wp-plugin.js', array(), $plugin_ver, true );
