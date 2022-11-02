b4193b6dbf5978a85f0cff80e13f4577faef1a8c
makeworld <makeworld@protonmail.com>
2022-11-02 11:58:50 -0400

    Make version updating easier

--- update_version.sh
+++ update_version.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i 's/0\.2\.9/0.2.10/g' block.json src/plugin.js package.json index.php
+# Escape old version for sed so 1.2.3 becomes 1\.2\.3
+# https://stackoverflow.com/a/54661050
+OLD_VERSION="$(echo "$1" | sed 's:[]\[^$.*/]:\\&:g')"
+sed -i "s/$OLD_VERSION/$2/g" block.json src/plugin.js package.json index.php
